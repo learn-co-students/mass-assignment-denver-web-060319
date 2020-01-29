49d944628d28846c78e7a9c7a9319114b61c6c98
Fekade Senbeta <fekadesenbeta@gmail.com>
2020-01-29 12:48:28 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,8 +2,8 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
 
-  def initialize(attributes)
-    attributes.map do |key, value|
+  def initialize(attrb)
+    attrb.map do |key, value|
       self.send(("#{key}="), value)
     end
   end
